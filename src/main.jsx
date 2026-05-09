7a1310a792ef5cde68ee9d9ee5729a844a0227b6
tonyopuodho <tonyopuodho@gmail.com>
2026-05-09 14:59:20 +0300

    Creating routes to different pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+import Service from './pages/Service'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"/Home",
+        element:<Home/>
+      },{
+        path:"/Blog",
+        element:<Blog/>
+      },{
+        path:"/Service",
+        element:<Service/>
+      },{
+        path:"/About",
+        element:<About/>
+      },{
+        path:"/Contact",
+        element:<Contact/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
