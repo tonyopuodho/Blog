8a7b1731ee2631a6ea99c7ad26089ebefb5ce3a8
tonyopuodho <tonyopuodho@gmail.com>
2026-05-11 15:36:32 +0300

    Implementing service page

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Service() {
   return (
-    <div>Service</div>
+    <div className='py-40 p-4 font-serif bg-black text-center text-white'>
+      <h1 className='text-5xl md:text-7xl font-extrabold'>Service Page</h1>
+    </div>
   )
 }
 
