a5a9d93d2944633a62fbdbab1b1009785f638505
tonyopuodho <tonyopuodho@gmail.com>
2026-05-11 15:22:41 +0300

    Implementing blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Blog() {
   return (
-    <div>Blog</div>
+    <div className='py-40 p-4 font-serif bg-black text-center text-white'>
+      <h1 className='text-5xl md:text-7xl font-extrabold'>Blog Page</h1>
+    </div>
   )
 }
 
