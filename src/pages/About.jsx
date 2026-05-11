66b4ae4483c0ba5101aa0474d05b59a10a052b39
tonyopuodho <tonyopuodho@gmail.com>
2026-05-11 15:34:46 +0300

    Implementing about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='py-40 p-4 font-serif bg-black text-center text-white'>
+      <h1 className='text-5xl md:text-7xl font-extrabold'>About Page</h1>
+    </div>
   )
 }
 
